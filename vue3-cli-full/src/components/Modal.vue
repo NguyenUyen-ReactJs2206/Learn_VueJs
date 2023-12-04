4e4734c0cbfc1bf8fc6a4354f2c004380e1fd49b
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-12-04 23:13:02 +0700

    using $emit

---
+++ vue3-cli-full/src/components/Modal.vue
@@ -0,0 +1,83 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="modal">
+    <div class="modal-box" :class="{ 'modal-bgpurple': theme === 'contact' }">
+      <div
+        class="modal-content"
+        :class="{ 'modal-color': theme === 'contact' }"
+      >
+        {{ title }}
+      </div>
+      <button @click="onCloseModal" class="modal-close">x</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Modal",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    theme: {
+      type: String,
+      // ko truyen pro thi mac dinh la contact
+      default: "contact",
+      //Truyen vao 1 trong nhung gia tri phia duoi
+      validator(value) {
+        return ["example", "sales", "contact"].includes(value);
+      },
+    },
+  },
+  methods: {
+    onCloseModal() {
+      this.$emit("close");
+    },
+  },
+  // data() {
+  //   return {
+  //     title: "This is title",
+  //   };
+  // },
+};
+</script>
+
+<style lang="scss">
+.modal {
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  background-color: rgb(21, 19, 19, 0.1);
+
+  &-box {
+    width: 200px;
+    height: 100px;
+    margin-top: 20vh;
+    margin-left: 30%;
+    background-color: rgba(255, 255, 255, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  &-content {
+    color: rgb(16, 16, 17);
+  }
+  &-close {
+    margin-left: 20px;
+    cursor: pointer;
+    padding: 5px 10px;
+    font-size: 18px;
+  }
+  &-bgpurple {
+    background-color: purple;
+  }
+
+  &-color {
+    color: rgb(255, 255, 255);
+  }
+}
+</style>
--- vue3-cli-full/src/views/AboutView.vue
+++ vue3-cli-full/src/views/AboutView.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Modal
+      v-if="isShowModal"
+      title="This is the Title"
+      v-on:close="onToggleModal"
+    />
+    <button @click="onToggleModal">Toggle Modal</button>
   </div>
 </template>
+
+<script>
+import Modal from "../components/Modal.vue";
+export default {
+  name: "AboutView",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      isShowModal: false,
+    };
+  },
+  methods: {
+    onToggleModal() {
+      this.isShowModal = !this.isShowModal;
+    },
+  },
+};
+</script>
+
+<style>
+.about {
+  position: relative;
+}
+</style>
