42dfb88814611b38bf374937ec9a739fd6cb4d25
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-12-05 13:51:41 +0700

    create card rotate 180 degrees

--- pokemon-minigame/src/components/Card.vue
+++ pokemon-minigame/src/components/Card.vue
@@ -31,4 +31,43 @@ export default {
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.card {
+  display: inline-block;
+  margin-right: 1rem;
+  margin-bottom: 1rem;
+  width: 90px;
+  height: 120px;
+
+  &__inner {
+    width: 100%;
+    height: 100%;
+    transition: transform 1s;
+    //quay dang 3d
+    transform-style: preserve-3d;
+    cursor: pointer;
+    position: relative;
+  }
+
+  &-is-flipped {
+    transform: rotateY(-180deg);
+  }
+
+  &__face {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    //quay tro thanh mat truoc va mac sau
+    backface-visibility: hidden;
+    overflow: hidden;
+    border-radius: 1rem;
+    padding: 1rem;
+    box-shadow: 0 3px 10px 3px rgba(0, 0, 0, 0.2);
+
+    &--back {
+      background-color: var(--light);
+      transform: rotateY(-180deg);
+    }
+  }
+}
+</style>
