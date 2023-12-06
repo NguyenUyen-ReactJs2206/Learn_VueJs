65e277735527c465c4ca53f2600a36f11e00e881
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-12-06 20:44:06 +0700

    test form binding

---
+++ vue3-cli-full/src/views/BindingForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+    <h1>BindingForm</h1>
+    <QuizForm />
+  </div>
+</template>
+
+<script>
+import QuizForm from "@/components/QuizForm.vue";
+
+export default {
+  name: "BindingForm",
+  methods: {},
+  components: { QuizForm },
+};
+</script>
--- vue3-cli-full/src/views/HomeView.vue
+++ vue3-cli-full/src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld ref="hello" msg="Welcome to Your Vue.js App" />
     <button @click="onChange">Change</button>
   </div>
